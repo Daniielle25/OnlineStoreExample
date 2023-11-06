0ef6a752afe13e1ee02b57fd41b0036b706aba6f
Daniielle25 <daniiellewood@gmail.com>
2023-11-06 15:13:16 -0800

    Adding Middleware

--- client/src/Features/catalog/Catalog.tsx
+++ client/src/Features/catalog/Catalog.tsx
@@ -1,3 +1,4 @@
+import agent from "../../App/API/agent";
 import { Product } from "../../App/Layout/Models/product";
 import ProductList from "./ProductList";
 import { useState, useEffect } from "react";
@@ -7,9 +8,7 @@ export default function Catalog() {
     const [products, setProducts] = useState<Product[]>([]);
 
     useEffect(() => {
-      fetch('http://localhost:5000/api/products')
-      .then(response => response.json())
-      .then(data => setProducts(data))
+      agent.Catalog.list().then(products => setProducts(products))
     }, [])
     
     return (
