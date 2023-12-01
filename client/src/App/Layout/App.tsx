38df7d1b95a20116546faf925e34f031790458ee
Daniielle25 <daniiellewood@gmail.com>
2023-12-01 13:39:28 -0800

    branch-07 commit

--- client/src/App/Layout/App.tsx
+++ client/src/App/Layout/App.tsx
@@ -2,10 +2,14 @@ import { useState } from "react";
 import Catalog from "../../Features/catalog/Catalog";
 import Header from "./header";
 import { Container, CssBaseline, ThemeProvider, createTheme } from "@mui/material";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
+import HomePage from "../../Features/home/HomePage";
 
 
 function App() {
+  const location=useLocation();
+  //const [loading, setLoading] = useState(true)
+
   const [darkMode, setDarkMode] = useState(false); 
   const paletteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -24,13 +28,20 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
     <CssBaseline/>
-      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} />
-      <Container>
+      <Header darkMode={darkMode} handleThemeChange={handleThemeChange} /> 
+      <Container sx={{mt: 4}}>
       <Outlet/>
       </Container>
-      
+            
     </ThemeProvider>
   ); 
 }
 
 export default App;
+
+// {loading ? <LoadingComponent message="Initialising app..."/>
+    //  : location.pathname === '/' ? <HomePage/>
+    //  : <Container sx={{mt: 4}}>
+    //  <Outlet/>
+    //  </Container>
+   // }
